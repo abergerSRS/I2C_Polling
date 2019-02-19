fcefc5f4498e7689b090785f1c0657bc9cee62f0
Andy Berger <aberger@thinksrs.com>
2019-02-19 09:22:03 -0800

    added angleComp table in preparation for sending uint32's to/from EEPROM

---
+++ utilities/angleComp.h
@@ -0,0 +1,11 @@
+/* ANGLECOMP_H */
+
+#ifndef ANGLECOMP_H
+#define ANGLECOMP_H
+
+#include <stdint.h>
+
+extern const uint32_t angleComp[100]; // defined in angleComp.c
+
+
+#endif /* ANGLECOMP_H */
